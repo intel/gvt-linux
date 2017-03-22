f082062d7e2f7edbc6ff99e12c3d4cbdad3e9371
Min He <min.he@intel.com>
2017-03-22 23:53:10 +0800

    drm/i915:Disable plane fault error interrupts
    In Xen Dom0, we always see the error interrupt from PIPE, so disable
    by not masking them.
    
    Signed-off-by: Min He <min.he@intel.com>

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -33,6 +33,7 @@
 #include <linux/circ_buf.h>
 #include <drm/drmP.h>
 #include <drm/i915_drm.h>
+#include <xen/xen.h>
 #include "i915_drv.h"
 #include "i915_trace.h"
 #include "intel_drv.h"
@@ -3428,9 +3429,13 @@ static void gen8_de_irq_postinstall(struct drm_i915_private *dev_priv)
 	u32 de_misc_masked = GEN8_DE_MISC_GSE;
 	enum pipe pipe;
 
-	if (INTEL_GEN(dev_priv) >= 9) {
-		de_pipe_masked |= GEN9_PIPE_PLANE1_FLIP_DONE |
-				  GEN9_DE_PIPE_IRQ_FAULT_ERRORS;
+	if (INTEL_INFO(dev_priv)->gen >= 9) {
+		if (xen_initial_domain())
+			de_pipe_masked |= GEN9_PIPE_PLANE1_FLIP_DONE;
+		else
+			de_pipe_masked |= GEN9_PIPE_PLANE1_FLIP_DONE |
+					  GEN9_DE_PIPE_IRQ_FAULT_ERRORS;
+
 		de_port_masked |= GEN9_AUX_CHANNEL_B | GEN9_AUX_CHANNEL_C |
 				  GEN9_AUX_CHANNEL_D;
 		if (IS_GEN9_LP(dev_priv))

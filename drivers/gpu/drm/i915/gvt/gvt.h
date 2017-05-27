18756b8b6e04f3592a326fa13555822c6dd1a8ba
Yulei Zhang <yulei.zhang@intel.com>
2017-05-27 16:51:18 +0800

    drm/i915/gvt: Implement vGPU status save and restore through new VFIO subregion VFIO_REGION_SUBTYPE_DEVICE_STATE
    Add read/write handler for VFIO mdev subregion
    VFIO_REGION_SUBTYPE_DEVICE_STATE, the first byte of
    the region is used to control the mdev device running status,
    the remaining are used for save/restore the internal states of
    mdev device.
    
    Signed-off-by: Yulei Zhang <yulei.zhang@intel.com>
    Signed-off-by: Xiao Zheng <xiao.zheng@intel.com>

--- drivers/gpu/drm/i915/gvt/gvt.h
+++ drivers/gpu/drm/i915/gvt/gvt.h
@@ -480,6 +480,8 @@ void intel_gvt_reset_vgpu_locked(struct intel_vgpu *vgpu, bool dmlr,
 void intel_gvt_reset_vgpu(struct intel_vgpu *vgpu);
 void intel_gvt_activate_vgpu(struct intel_vgpu *vgpu);
 void intel_gvt_deactivate_vgpu(struct intel_vgpu *vgpu);
+int intel_gvt_save_restore(struct intel_vgpu *vgpu, char *buf, size_t count,
+			   void *base, uint64_t off, bool restore);
 
 /* validating GM functions */
 #define vgpu_gmadr_is_aperture(vgpu, gmadr) \
@@ -576,6 +578,8 @@ struct intel_gvt_ops {
 			struct attribute_group ***intel_vgpu_type_groups);
 	int (*vgpu_query_plane)(struct intel_vgpu *vgpu, void *);
 	int (*vgpu_get_dmabuf)(struct intel_vgpu *vgpu, unsigned int);
+	int  (*vgpu_save_restore)(struct intel_vgpu *, char *buf, size_t count,
+				  void *base, uint64_t off, bool restore);
 };
 
 

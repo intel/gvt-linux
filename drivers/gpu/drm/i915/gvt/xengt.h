9174369bdb15e326ca139ae48e9c80b1a624e31b
Yulei Zhang <yulei.zhang@intel.com>
2017-08-31 09:55:16 +0800

    drm/i915/gvt: Add log dirty support for XENGT migration
    Keep tracking the DMA buffer pages used by vGPU in kernel
    device module, and notify hypervisor to dirty these pages
    for live migration.
    
    Signed-off-by: Yulei Zhang <yulei.zhang@intel.com>

--- drivers/gpu/drm/i915/gvt/xengt.h
+++ drivers/gpu/drm/i915/gvt/xengt.h
@@ -44,6 +44,8 @@ struct xengt_hvm_dev {
 	/* for >1MB memory of HVM: each vm_struct means 4KB */
 	struct vm_struct **vmem_vma_4k;
 	void *dev_state;
+	struct rb_root logd_list;
+	struct mutex logd_lock;
 };
 
 struct xengt_hvm_params {

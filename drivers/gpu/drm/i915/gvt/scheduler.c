6fb7001dab9767b4f31f73050abd7a3b65aec614
Zhenyu Wang <zhenyuw@linux.intel.com>
2017-11-14 11:33:08 +0800

    STASH: Fix up for commit d2b4b979
    Fix up build after
    
    commit d2b4b97933f5adacfba42dc3b9200d0e21fbe2c4
    Author: Chris Wilson <chris@chris-wilson.co.uk>
    Date:   Fri Nov 10 14:26:33 2017 +0000
    
        drm/i915: Record the default hw state after reset upon load
    
    need to refresh gvt tree later.

--- drivers/gpu/drm/i915/gvt/scheduler.c
+++ drivers/gpu/drm/i915/gvt/scheduler.c
@@ -1014,8 +1014,6 @@ int intel_vgpu_setup_submission(struct intel_vgpu *vgpu)
 	if (IS_ERR(s->shadow_ctx))
 		return PTR_ERR(s->shadow_ctx);
 
-	s->shadow_ctx->engine[RCS].initialised = true;
-
 	bitmap_zero(s->shadow_ctx_desc_updated, I915_NUM_ENGINES);
 
 	s->workloads = kmem_cache_create("gvt-g_vgpu_workload",

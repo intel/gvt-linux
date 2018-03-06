01fe156c504a4a6c5e8aa73ffff3c6e1ae787f6d
Xiong Zhang <xiong.y.zhang@intel.com>
2018-03-06 22:06:12 +0800

    drm/i915/gvt/xengt: Impliment mpt->dma_map/unmap_guest_page
    Commit b2079a4352a7(drm/i915/gvt: Fix guest vGPU hang caused by very
    high dma setup overhead) add a mandatory mpt interface
    dma_map/unmap_guest_page, xengt should implement it. it has the same
    function as gfn_to_mfn in xengt.
    
    Signed-off-by: Xiong Zhang <xiong.y.zhang@intel.com>

--- drivers/gpu/drm/i915/gvt/xengt.c
+++ drivers/gpu/drm/i915/gvt/xengt.c
@@ -1810,6 +1810,26 @@ static unsigned long xengt_gfn_to_pfn(unsigned long handle, unsigned long gfn)
 	return pfn;
 }
 
+static int xengt_dma_map_guest_page(unsigned long handle, unsigned long gfn,
+				    unsigned long size, dma_addr_t *dma_addr)
+{
+	unsigned long pfn;
+
+	pfn = xengt_gfn_to_pfn(handle, gfn);
+
+	if (pfn < 0)
+		return -EINVAL;
+
+	*dma_addr = pfn << PAGE_SHIFT;
+
+	return 0;
+}
+
+static void xengt_dma_unmap_guest_page(unsigned long handle,
+			dma_addr_t dma_addr)
+{
+}
+
 static struct intel_gvt_mpt xengt_mpt = {
 	//.detect_host = xengt_detect_host,
 	.type = INTEL_GVT_HYPERVISOR_XEN,
@@ -1824,6 +1844,8 @@ static struct intel_gvt_mpt xengt_mpt = {
 	.read_gpa = xengt_read_gpa,
 	.write_gpa = xengt_write_gpa,
 	.gfn_to_mfn = xengt_gfn_to_pfn,
+	.dma_map_guest_page = xengt_dma_map_guest_page,
+	.dma_unmap_guest_page = xengt_dma_unmap_guest_page,
 	.map_gfn_to_mfn = xengt_map_gfn_to_mfn,
 	.set_trap_area = xengt_set_trap_area,
 };

801ef8bfaa2ea2c9ed2df1f7a72d8ee75bcc4c32
Xiong Zhang <xiong.y.zhang@intel.com>
2018-09-15 00:01:34 +0800

    drm/i915/gvt: Fix a command corruption caused by live migration patch
    Patch "72060b2c0969 drm/i915/gvt: Patch the gma in gpu commands during
    command parser" will patch gma address in command buffer, this patch will
    clear gma_address[1:0] and set gma_address[31:0]. But some command use
    gma_address[2] and gma_address[1], then this patch will corrupt these
    command. Such as MI_FLUSH_DW use gma_address[2] to store gtt or ppgtt
    address space, MI_STORE_DATA_IMM use gam_address[1] to store an offset
    flag.
    
    This patch add a mask parameter at patch gma_address.
    
    Signed-off-by: Xiong Zhang <xiong.y.zhang@intel.com>

--- drivers/gpu/drm/i915/gvt/cmd_parser.c
+++ drivers/gpu/drm/i915/gvt/cmd_parser.c
@@ -969,7 +969,10 @@ static int cmd_handler_lrr(struct parser_exec_state *s)
 
 static inline int cmd_address_audit(struct parser_exec_state *s,
 				    unsigned long guest_gma, int op_size,
-				    bool index_mode, int offset);
+				    bool index_mode);
+static inline void patch_gma(struct parser_exec_state *s,
+			     unsigned long guest_gma, unsigned int offset,
+			     unsigned int mask);
 
 static int cmd_handler_lrm(struct parser_exec_state *s)
 {
@@ -991,10 +994,10 @@ static int cmd_handler_lrm(struct parser_exec_state *s)
 			gma = cmd_gma(s, i + 1);
 			if (gmadr_bytes == 8)
 				gma |= (cmd_gma_hi(s, i + 2)) << 32;
-			ret |= cmd_address_audit(s, gma, sizeof(u32),
-						 false, i + 1);
+			ret |= cmd_address_audit(s, gma, sizeof(u32), false);
 			if (ret)
 				break;
+			patch_gma(s, gma, i + 1, GENMASK(31, 2));
 		}
 		i += gmadr_dw_number(s) + 1;
 	}
@@ -1016,10 +1019,11 @@ static int cmd_handler_srm(struct parser_exec_state *s)
 			gma = cmd_gma(s, i + 1);
 			if (gmadr_bytes == 8)
 				gma |= (cmd_gma_hi(s, i + 2)) << 32;
-			ret |= cmd_address_audit(s, gma, sizeof(u32),
-						 false, i + 1);
+			ret |= cmd_address_audit(s, gma, sizeof(u32), false);
 			if (ret)
 				break;
+
+			patch_gma(s, gma, i + 1, GENMASK(31, 2));
 		}
 		i += gmadr_dw_number(s) + 1;
 	}
@@ -1089,7 +1093,9 @@ static int cmd_handler_pipe_control(struct parser_exec_state *s)
 				if (cmd_val(s, 1) & (1 << 21))
 					index_mode = true;
 				ret |= cmd_address_audit(s, gma, sizeof(u64),
-						index_mode, 2);
+							 index_mode);
+				if (!ret)
+					patch_gma(s, gma, 2, GENMASK(31, 2));
 			}
 		}
 	}
@@ -1418,14 +1424,29 @@ static unsigned long get_gma_bb_from_cmd(struct parser_exec_state *s, int index)
 	return addr;
 }
 
+static inline void patch_gma(struct parser_exec_state *s,
+			     unsigned long guest_gma, unsigned int offset,
+			     unsigned int mask)
+{
+	struct intel_vgpu *vgpu = s->vgpu;
+	int gmadr_bytes = vgpu->gvt->device_info.gmadr_bytes_in_cmd;
+	u64 host_gma;
+
+	intel_gvt_ggtt_gmadr_g2h(vgpu, guest_gma, &host_gma);
+
+	patch_value(s, cmd_ptr(s, offset),
+		    (host_gma & mask) | (cmd_val(s, offset) & (~mask)));
+	if (gmadr_bytes == 8)
+		patch_value(s, cmd_ptr(s, offset + 1),
+			    (host_gma >> 32) & GENMASK(15, 0));
+}
+
 static inline int cmd_address_audit(struct parser_exec_state *s,
 				    unsigned long guest_gma, int op_size,
-				    bool index_mode, int offset)
+				    bool index_mode)
 {
 	struct intel_vgpu *vgpu = s->vgpu;
 	u32 max_surface_size = vgpu->gvt->device_info.max_surface_size;
-	int gmadr_bytes = vgpu->gvt->device_info.gmadr_bytes_in_cmd;
-	u64 host_gma;
 	int i;
 	int ret;
 
@@ -1443,14 +1464,6 @@ static inline int cmd_address_audit(struct parser_exec_state *s,
 	} else if (!intel_gvt_ggtt_validate_range(vgpu, guest_gma, op_size)) {
 		ret = -EFAULT;
 		goto err;
-	} else
-		intel_gvt_ggtt_gmadr_g2h(vgpu, guest_gma, &host_gma);
-
-	if (offset > 0) {
-		patch_value(s, cmd_ptr(s, offset), host_gma & GENMASK(31, 2));
-		if (gmadr_bytes == 8)
-			patch_value(s, cmd_ptr(s, offset + 1),
-				(host_gma >> 32) & GENMASK(15, 0));
 	}
 
 	return 0;
@@ -1495,7 +1508,11 @@ static int cmd_handler_mi_store_data_imm(struct parser_exec_state *s)
 		gma = (gma_high << 32) | gma_low;
 		core_id = (cmd_val(s, 1) & (1 << 0)) ? 1 : 0;
 	}
-	ret = cmd_address_audit(s, gma + op_size * core_id, op_size, false, 1);
+
+	ret = cmd_address_audit(s, gma + op_size * core_id, op_size, false);
+	if (!ret)
+		patch_gma(s, gma, 1, GENMASK(31, 2));
+
 	return ret;
 }
 
@@ -1539,7 +1556,10 @@ static int cmd_handler_mi_op_2f(struct parser_exec_state *s)
 		gma_high = cmd_val(s, 2) & GENMASK(15, 0);
 		gma = (gma_high << 32) | gma;
 	}
-	ret = cmd_address_audit(s, gma, op_size, false, 1);
+	ret = cmd_address_audit(s, gma, op_size, false);
+	if (!ret)
+		patch_gma(s, gma, 1, GENMASK(31, 2));
+
 	return ret;
 }
 
@@ -1579,9 +1599,11 @@ static int cmd_handler_mi_flush_dw(struct parser_exec_state *s)
 		/* Store Data Index */
 		if (cmd_val(s, 0) & (1 << 21))
 			index_mode = true;
-		ret = cmd_address_audit(s, (gma | (1 << 2)),
-					sizeof(u64), index_mode, 1);
+		ret = cmd_address_audit(s, gma, sizeof(u64), index_mode);
+		if (!ret)
+			patch_gma(s, gma, 1, GENMASK(31, 3));
 	}
+
 	/* Check notify bit */
 	if ((cmd_val(s, 0) & (1 << 8)))
 		set_bit(cmd_interrupt_events[s->ring_id].mi_flush_dw,

51af8937d730179613babc88945205235c63d17b
Chris Wilson <chris@chris-wilson.co.uk>
2019-04-25 10:19:52 +0100

    lockdep: Swap storage for pin_count and references
    As a lockmap takes a reference for every ww_mutex used together, this
    can be an arbitrarily large number and under control of userspace --
    easily overflowing the arbitrary limit of 4096. However, the pin_count
    (used for detecting unexpected lock dropping) is a full 32b despite
    nesting being extremely rare (see lockdep_pin_lock).
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190425092004.9995-33-chris@chris-wilson.co.uk

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -4755,11 +4755,14 @@ static struct pin_cookie __lock_pin_lock(struct lockdep_map *lock)
 
 		if (match_held_lock(hlock, lock)) {
 			/*
-			 * Grab 16bits of randomness; this is sufficient to not
-			 * be guessable and still allows some pin nesting in
-			 * our u32 pin_count.
+			 * Grab 6bits of randomness; this is barely sufficient
+			 * to not be guessable and still allows some 32 levels
+			 * of pin nesting in our u12 pin_count.
 			 */
-			cookie.val = 1 + (prandom_u32() >> 16);
+			cookie.val = 1 + (prandom_u32() >> 26);
+			if (DEBUG_LOCKS_WARN_ON(hlock->pin_count + cookie.val >= 1 << 12))
+				return NIL_COOKIE;
+
 			hlock->pin_count += cookie.val;
 			return cookie;
 		}

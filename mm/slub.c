c34186d3faf4a994d6c70dda5a539303342dc0e2
Chris Wilson <chris@chris-wilson.co.uk>
2019-07-01 15:29:03 +0100

    mm: Show slab debug as offsets from section base not hashed pointers
    Since the kernel now used hashed pointers for raw addresses, it is very
    hard to guage the relative placement within a section, and since the
    hash value will never match up with any contents, using it provides no
    information relevant for slab debugging. Show the relative offset into
    each section, so that some reference for the hexdump is provided.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- mm/slub.c
+++ mm/slub.c
@@ -546,7 +546,7 @@ static void print_section(char *level, char *text, u8 *addr,
 			  unsigned int length)
 {
 	metadata_access_enable();
-	print_hex_dump(level, text, DUMP_PREFIX_ADDRESS, 16, 1, addr,
+	print_hex_dump(level, text, DUMP_PREFIX_OFFSET, 16, 1, addr,
 			length, 1);
 	metadata_access_disable();
 }

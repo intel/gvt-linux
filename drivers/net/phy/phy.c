5e83d0e7689e05bf840c951239597be557d870de
Chris Wilson <chris@chris-wilson.co.uk>
2020-12-17 16:47:00 +0000

    HAX net/phy: Suppress WARN for calling stop while halted
    Closes: https://gitlab.freedesktop.org/drm/intel/-/issues/2805

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -1005,11 +1005,8 @@ void phy_stop(struct phy_device *phydev)
 {
 	struct net_device *dev = phydev->attached_dev;
 
-	if (!phy_is_started(phydev) && phydev->state != PHY_DOWN) {
-		WARN(1, "called from state %s\n",
-		     phy_state_to_str(phydev->state));
+	if (!phy_is_started(phydev) && phydev->state != PHY_DOWN)
 		return;
-	}
 
 	mutex_lock(&phydev->lock);
 
